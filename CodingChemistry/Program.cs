333d6bb026c3f55cd8e5957f855a3bfc7e67d638
Bernard Estephan <estephan23@gmail.com>
2023-11-21 22:38:25 -0500

    updated while loop to allow user to input after null value

--- CodingChemistry/Program.cs
+++ CodingChemistry/Program.cs
@@ -16,30 +16,38 @@ namespace CodingChemistry
             SeedTheDataBase();
             {
                 // Get user input on which element they want to see.
-                Console.WriteLine("Hello, welcome to Mr. Estephan's Coding Chemistry Program!\nPlease enter the symbol of the element you would like to explore!");
+                Console.WriteLine("Hello, welcome to Mr. Estephan's 'Coding Chemistry' Program!\n\nPlease enter the symbol of the element you would like to explore!");
                 string userInput = Console.ReadLine() ?? "";
 
                 // Get the element from the database.
-                Element? E = context.Elements.FromSqlRaw($"SELECT * FROM Elements WHERE Symbol = '{userInput}'").SingleOrDefault();
-                if (E == null)
-                {
-                    Console.WriteLine("Uhoh! " + userInput + " is not an element.");
-                    Console.WriteLine("Double check your spelling and capitalization then try again!");
-                    Console.WriteLine("Please enter the symbol of the element you would like to explore!");
-                    userInput = Console.ReadLine() ?? ""; 
-                }
-                
-                // Print information regarding the element to the console.
-                Console.WriteLine("The element you selected is " + E?.Name + ". " + E?.Name + " has an atomic number of " + E?.AtomicNumber + ".  An atomic number of " + E?.AtomicNumber + " means it contains " + E?.AtomicNumber + " protons.  Protons are subatomic particles that are located in the nucleus of the atom and contain a positive(+) charge.  The number of protons in an element is what makes an element unique.  Only " + E?.Name + " contains exactly " + E.AtomicNumber + " protons.");
-                Console.WriteLine("\nPress 1 to learn about the Atomic mass of " + E?.Name + " .");
-                Console.WriteLine("\nPress 2 to learn about the location of " + E?.Name + " and the type of element it is.");
-                Console.WriteLine("\nPress 3 to learn about the electron configuration of " + E?.Name);
-                Console.WriteLine("\nPress 'exit' to quit.");
+                Element? Element = context.Elements.FromSqlRaw($"SELECT * FROM Elements WHERE Symbol = '{userInput}'").SingleOrDefault();
+                Element E = Element;
 
+                while (E == null)
+                {
+                    Console.WriteLine("\nUhoh! " + "'" + userInput + "'" + " is not an element.  Double check your spelling, capitalization and try again!");
+                   
+                    Console.WriteLine("\nPlease enter the symbol of the element you would like to explore!");
+                    userInput = Console.ReadLine() ?? "";
+                    Element = context.Elements.FromSqlRaw($"SELECT * FROM Elements WHERE Symbol = '{userInput}'").SingleOrDefault();
+                    E = Element;
+                }
+                // Print information regarding the element to the console.
+                if (E != null)
+                {
+                    Console.WriteLine("\nThe element you selected is " + E?.Name + ". " + E?.Name + " has an atomic number of " + E?.AtomicNumber + ".  An atomic number of " + E?.AtomicNumber + " means it contains " + E?.AtomicNumber + " protons.  Protons are subatomic particles that are located in the nucleus of  atom and contain a positive(+) charge.  The number of protons in an element is what makes an element unique.  Only " + E?.Name + " contains exactly " + E.AtomicNumber + " protons.");
+                    Console.WriteLine("\n\n\nPress 1 to learn about the Atomic Mass of " + E?.Name + " .");
+                    Console.WriteLine("Press 2 to learn about the location of " + E?.Name + " and the type of element it is.");
+                    Console.WriteLine("Press 3 to learn about the electron configuration of " + E?.Name);
+                    userInput = Console.ReadLine() ?? "";
+                    //Console.WriteLine("\nPress 'exit' to quit.");
+                }
+           
                 while (userInput.ToLower() != "exit")
                 {
                     if (userInput == "1")
                     {
+                        
                         Console.WriteLine("The atomic mass for " + E.Name + " is " + E.AtomicMass);
                         Console.WriteLine("The atomic mass of an element is in amu, atomic mass units.  The atomic mass consists of the mass of all the protons and neutrons in that element.");
                         Console.WriteLine("The atomic mass tells us the number of protons and neutrons in an element. In this case, since " + E.Name + " has " + E.AtomicNumber + " protons, that means if you subtract the " + E.AtomicNumber + " from, the " + E.AtomicMass + " you will get the number of neturons, after you round to the nearest whole number.");
@@ -59,6 +67,8 @@ namespace CodingChemistry
                     
                     userInput = Console.ReadLine();
                 }
+                Console.WriteLine("\nPress 'exit' to quit.");
+
             }
         }
         private static void SeedTheDataBase()
